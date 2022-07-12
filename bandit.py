231f17f397d14f33d9cc854e6563a9dac7e9024d
zt#7380 <98675255+x8g@users.noreply.github.com>
2022-07-12 01:06:17 -0400

    updated fix headers.

--- bandit.py
+++ bandit.py
@@ -12,8 +12,9 @@ os.system('title Bandit Username Checker')
 with open('usernames.txt', 'r', encoding='UTF-8', errors='replace') as u:
     usernames = u.read().splitlines()
     all_usernames = len(usernames)
-    sid = input(Colorate.Horizontal(Colors.blue_to_purple, 'Sid?: '))
-    threads = input(Colorate.Horizontal(Colors.blue_to_purple, 'Threads? (5 or less): '))
+    
+    
+threads = input(Colorate.Horizontal(Colors.blue_to_purple, 'Threads? (5 or less): '))
 # ----
 def check():
     while True:
@@ -23,13 +24,13 @@ def check():
           "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Mobile Safari/537.36",
           "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9 ccept-encoding: gzip, deflate, br",
           "scheme": "https",
-          "cookie": f"sessionid={sid}" }
+          "cookie": f"sessionid=cf4ab1e774b525a7b43328733062e9d1" 
+          }
       checkreq = requests.get(checkurl, headers=checkhed)
-      print(checkreq.text)
       if '"statusCode":10202' in (checkreq.text):
             print(Colorate.Horizontal(Colors.blue_to_purple, 'Webhook Sent | Avail User! | ' + username))
             webhook = DiscordWebhook(url=url, content=f'``{username}`` is available!')
-            webhook = execute()
+            response = webhook.execute()
             with open('available.txt', "a") as x:
              x.write(f"{username}\n")
       if '"statusCode":0' in (checkreq.text):
